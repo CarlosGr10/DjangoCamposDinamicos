914154c31900fc48bf958a0e25c1d47e8b095963
Carlos Garcia <cgarcia@raloy.com.mx>
2020-09-01 08:17:56 -0500

    Inputs formset no dimamicos

--- product/models.py
+++ product/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class ModelItem(models.Model):
+    item = models.CharField(max_length=20)
+    state = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.item
+
+
 class ModelProduct(models.Model):
     name = models.CharField(max_length=30)
     code = models.CharField(max_length=30)
